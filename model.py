c51ae23e7b916efa52859bfed1d3b50caad977d1
e <chengyingjuly@163.com>
2020-05-05 13:27:34 +0800

    fix bug

--- model.py
+++ model.py
@@ -238,7 +238,7 @@ class SELayer(nn.Module):
 def getModel(config):
     model = None
     if config.data_fe == 'shifted_proba':
-        input_size = 26
+        input_size = 15
     elif config.data_fe == 'shifted':
         input_size = 8
     else:

8f6e9261cebcccf2f6a11eea8de9c772993f4ed9
cheng ying <chengying@tezign.com>
2020-05-06 16:38:37 +0800

    add cbr

--- train.py
+++ train.py
@@ -207,10 +207,20 @@ if __name__ == '__main__':
 
 
     # shift 1,2,3
-    config = buildConfig(0)
-    config.expriment_id = 95
-    config.data_fe = 'shifted_viterbi_proba'
+    # config = buildConfig(0)
+    # config.expriment_id = 95
+    # config.data_fe = 'shifted_viterbi_proba'
+    #
+    # config.loss = 'ce'
+    # config.schedular = 'cyc'
+    # config.early_stop_max = True
+    # config.use_cbr = True
+    # config.drop_out = 0.5
+    # config_list.append(config)
 
+    config = buildConfig(0)
+    config.expriment_id = 96
+    config.data_fe = 'shifted'
     config.loss = 'ce'
     config.schedular = 'cyc'
     config.early_stop_max = True

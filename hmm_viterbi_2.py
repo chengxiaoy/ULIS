1dc7170ebb453f9647a1372846bde47279cdcc15
cheng ying <chengying@tezign.com>
2020-05-09 18:03:37 +0800

    add cbr

--- hmm_viterbi_2.py
+++ hmm_viterbi_2.py
@@ -45,28 +45,27 @@ import joblib
 from pomegranate import *
 
 import numpy as np
-
-X = []
-Z = []
-for i in range(100):
-    z = np.random.randint(0, 5, size=100)
-    x = []
-    for j in z:
-        x.append(np.random.normal(j, 0.1 + 0.1 * j))
-    X.append(x)
-    Z.append(z)
-X = np.array(X)
-# X = X.reshape(100,1000,-1)
-
-
-model = HiddenMarkovModel.from_samples(NormalDistribution, n_components=5, X=X[:20])
-viterbi_state = model.viterbi(X[50])
-print(Z[50])
-print(", ".join(state.name for i, state in viterbi_state[1]))
+#
+# X = []
+# Z = []
+# for i in range(100):
+#     z = np.random.randint(0, 5, size=100)
+#     x = []
+#     for j in z:
+#         x.append(np.random.normal(j, 0.1 + 0.1 * j))
+#     X.append(x)
+#     Z.append(z)
+# X = np.array(X)
+# # X = X.reshape(100,1000,-1)
+#
+#
+# model = HiddenMarkovModel.from_samples(NormalDistribution, n_components=5, X=X[:20])
+# viterbi_state = model.viterbi(X[50])
+# print(Z[50])
+# print(", ".join(state.name for i, state in viterbi_state[1]))
 
 import helper
 import pandas as pd
-
 
 train_states, train_signals, train_groups, test_signals, test_groups = helper.load_data(kalman_filter=False)
 test_y_pred = [None] * np.sum([len(x) for x in test_groups])
@@ -83,11 +82,15 @@ for index, (train_group, test_group) in enumerate(zip(train_groups, test_groups)
     model = HiddenMarkovModel.from_samples(NormalDistribution, n_components=len(np.unique(true_state_train)),
                                            X=signal_train)
 
-
-
     for test_grp in test_group:
-        test_signals[test_grp] =
-        test_y_pred[test_grp] = model.decoding(test_signals[test_grp], test_grp)
+        test_list = test_signals[test_grp].reshape(-1, 4000)
+        state_list = []
+        for test in test_list:
+            state = model.viterbi(test)
+            for i, s in state[1][1:]:
+                state_list.extend(i)
+        print(len(state_list))
+        test_y_pred[test_grp] = state_list
     print("cost {} s".format(time.time() - since))
 
 test_y_pred = np.concatenate(test_y_pred)
